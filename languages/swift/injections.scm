ed61c8af230f69acaa26c1382852d9355cfa29b6
Dylan Vann <dylan@dylanvann.com>
2026-09-24 14:17:31 +0700

    Inject Markdown into documentation comments
    Xcode renders /// and /** */ comments as documentation markup, coloring code
    spans, emphasis, and links. Injecting markdown-inline gives themes the same
    hooks. Plain comments keep the comment injection.

--- languages/swift/injections.scm
+++ languages/swift/injections.scm
@@ -2,8 +2,19 @@
 ((regex_literal) @injection.content
   (#set! injection.language "regex"))
 
-([
-  (comment)
-  (multiline_comment)
-] @injection.content
+; Documentation comments are Markdown, as Xcode renders them.
+((comment) @injection.content
+  (#match? @injection.content "^///")
+  (#set! injection.language "markdown-inline"))
+
+((multiline_comment) @injection.content
+  (#match? @injection.content "^/\\*\\*")
+  (#set! injection.language "markdown-inline"))
+
+((comment) @injection.content
+  (#not-match? @injection.content "^///")
+  (#set! injection.language "comment"))
+
+((multiline_comment) @injection.content
+  (#not-match? @injection.content "^/\\*\\*")
   (#set! injection.language "comment"))
